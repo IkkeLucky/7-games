96e718d90f98b7fd86c65590f88b73c6ddb3ee9a
ikkelucky <nachomarchesini@gmail.com>
2022-10-21 13:02:20 -0300

    Click logic to match imgs so far

--- memoryGame.js
+++ memoryGame.js
@@ -44,14 +44,45 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard () {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/question.jpg')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipcard)
         gridDisplay.appendChild(card)
     }
 }
 
 createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+
+    console.log('check for match!')
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match!')
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/complete.jpg')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/complete.jpg')
+        cards[cardsChosenIds[0].removeEventListener('click', flipcard)]
+        cards[cardsChosenIds[1].removeEventListener('click', flipcard)]
+        cardsWon.push(cardsChosen)
+    }
+    cardsChosen = []
+    cardsChosenIds = []
+}
+
+function flipcard(){
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    // Now to check if they match
+    if (cardsChosen.length === 2){
+        setTimeout(checkMatch , 500)
+    }
+}
