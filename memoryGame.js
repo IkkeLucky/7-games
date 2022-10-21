945fda7448a420859966e16ed1b7a50ae0e8abdb
ikkelucky <nachomarchesini@gmail.com>
2022-10-21 12:31:59 -0300

    Cards array

--- memoryGame.js
+++ memoryGame.js
@@ -1,6 +1,57 @@
 const cardArray = [
     {
         name: 'newYork',
-        img: '',
+        img: 'images/newYork.jpg',
+    },
+    {
+        name: 'copenhague',
+        img: 'images/copenhague.jpg'
+    },
+    {
+        name: 'london',
+        img: 'images/london.jpg',
+    },
+    {
+        name: 'rio',
+        img: 'images/rio.jpg',
+    },
+    {
+        name: 'argy',
+        img: 'images/argy.jpg',
+    },
+    {
+        name: 'newYork',
+        img: 'images/newYork.jpg',
+    },
+    {
+        name: 'copenhague',
+        img: 'images/copenhague.jpg'
+    },
+    {
+        name: 'london',
+        img: 'images/london.jpg',
+    },
+    {
+        name: 'rio',
+        img: 'images/rio.jpg',
+    },
+    {
+        name: 'argy',
+        img: 'images/argy.jpg',
     }
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard () {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/question.jpg')
+        card.setAttribute('data-id', i)
+        gridDisplay.appendChild(card)
+    }
+}
+
+createBoard()
