ce873c39e7489213a093e781b515a3fd8aa1107f
ikkelucky <nachomarchesini@gmail.com>
2023-04-03 19:53:23 -0300

    movement to the logs

--- frogger.js
+++ frogger.js
@@ -2,6 +2,9 @@ const timeLeftDisplay = document.querySelector('#timeLeft')
 const resultDisplay = document.querySelector('#result')
 const startPauseDisplay = document.querySelector('#startPause')
 const squares = document.querySelectorAll('.grid div')
+const logsleft = document.querySelectorAll('.logLeft')
+
+
 
 let currentIndex = 76
 const width = 9
@@ -37,3 +40,47 @@ function moveFrog(e) {
 }
 
 document.addEventListener('keyup', moveFrog)
+
+
+
+
+function autoMoveLogs() {
+    logsleft.forEach( logLeft => moveLogLeft (logLeft) )
+}
+autoMoveLogs
+
+
+function moveLogLeft (logLeft) {
+    switch(true) {
+        case logLeft.classList.contains('l1') :
+            logLeft.classList.remove('l1')
+            logLeft.classList.add('l2')
+            break
+        case logLeft.classList.contains('l2') :
+            logLeft.classList.remove('l2')
+            logLeft.classList.add('l3')
+            break
+        case logLeft.classList.contains('l3') :
+            logLeft.classList.remove('l3')
+            logLeft.classList.add('l4')
+            break
+        case logLeft.classList.contains('l4') :
+            logLeft.classList.remove('l4')
+            logLeft.classList.add('l5')
+            break
+        case logLeft.classList.contains('l5') :
+            logLeft.classList.remove('l5')
+            logLeft.classList.add('l1')
+            break
+    }
+}
+
+setInterval(autoMoveLogs,1000)
+
+
+
+
+
+
+
+
